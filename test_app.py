6e5af4ea3719720249b26aeba7dee8ab66c4eadf
Andrew Mitchell <andymitch559@gmail.com>
2021-08-25 16:30:12 -0600

    s

--- test_app.py
+++ test_app.py
@@ -1,10 +1,26 @@
-from flask import Flask, send_from_directory, render_template
-import os
+from flask import Flask
 
-app = Flask(__name__,template_folder='template')
+
+ui = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="{{ url_for('static', filename='favicon.ico') }}">
+    <title>Yuzu</title>
+</head>
+<body>
+    <h1>Hello!</h1>
+</body>
+</html>
+'''
+
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('hello.html')
+    return ui
 
 app.run(host='0.0.0.0', port=5000, debug=True)
