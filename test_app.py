0938e70b47164ca64f9e397f478e2c63fe989610
andymitch <andymitch559@gmail.com>
2021-08-25 23:52:10 +0100

    icon

--- test_app.py
+++ test_app.py
@@ -8,7 +8,7 @@ ui = '''
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="{{ url_for('static', filename='favicon.ico') }}">
+    <link rel="icon" type="image/png" href="https://andymitch.github.io/yuzu-ghost-icon.png">
     <title>Yuzu</title>
 </head>
 <body>
