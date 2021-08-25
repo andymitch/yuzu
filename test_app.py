fddd16077ecb637ba8dc581db83d3e9c35371e1b
Andrew Mitchell <andymitch559@gmail.com>
2021-08-25 16:26:13 -0600

    g

--- test_app.py
+++ test_app.py
@@ -1,14 +1,10 @@
 from flask import Flask, send_from_directory, render_template
 import os
 
-app = Flask('yaza')
+app = Flask(__name__,template_folder='template')
 
 @app.route("/")
 def hello():
     return render_template('hello.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=5000, debug=True)
